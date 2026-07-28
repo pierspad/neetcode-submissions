140304fb33c287d3558bdd5d50962c455f6e5105
pierspad <75368540+pierspad@users.noreply.github.com>
2026-07-28 12:12:17 +0200

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+  std::vector<std::vector<std::string>>
+  groupAnagrams(std::vector<std::string> &strs) {
+
+    std::vector<std::vector<std::string>> sol;
+
+    std::unordered_map<std::string, std::vector<std::string>> map;
+    std::string aux = "";
+    for (auto s : strs) {
+      aux = s;
+      sort(aux.begin(), aux.end());
+      map[aux].push_back(s);
+    }
+
+    for (auto it = map.begin(); it != map.end(); it++) {
+      sol.push_back(it->second);
+    }
+
+    return sol;
+  }
+};
